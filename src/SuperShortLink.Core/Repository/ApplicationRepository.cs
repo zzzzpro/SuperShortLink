40f17c0cc12407d50fa9b3946e750ad3e2d79988
sbzj <z@zhaojin.me>
2024-02-21 13:16:25 +0800

    修正单词拼写错误

--- src/SuperShortLink.Core/Repository/ApplicationRepository.cs
+++ src/SuperShortLink.Core/Repository/ApplicationRepository.cs
@@ -18,7 +18,7 @@ namespace SuperShortLink.Repository
 
         public async Task<PageResponseDto<ApplicationModel>> QueryPageAsync(ApplicationListRequest dto)
         {
-            var sb = new StringBuilder($"select * from short_link_appication where status={StatusEnum.Valid.GetHashCode()} ");
+            var sb = new StringBuilder($"select * from short_link_application where status={StatusEnum.Valid.GetHashCode()} ");
 
             if (!string.IsNullOrEmpty(dto.app_code))
             {
@@ -33,25 +33,24 @@ namespace SuperShortLink.Repository
             }
 
             var orderSql = " order by app_id desc";
-
             return await base.PageQueryAsync<ApplicationModel>(sb.ToString(), dto, orderSql, dto);
         }
 
         public async Task<ApplicationModel> GetByCodeAsync(string appCode)
         {
-            var sql = $"select * from short_link_appication where  app_code = {_paramSql}appCode and status = {_paramSql}status limit 1";
+            var sql = $"select * from short_link_application where  app_code = {_paramSql}appCode and status = {_paramSql}status limit 1";
             return await base.QueryFirstOrDefaultAsync<ApplicationModel>(sql, new { appCode, status = StatusEnum.Valid.GetHashCode() });
         }
 
         public async Task<ApplicationModel> GetByIdAsync(int appId)
         {
-            var sql = $"select * from short_link_appication where  app_id = {_paramSql}appId and status = {_paramSql}status limit 1";
+            var sql = $"select * from short_link_application where  app_id = {_paramSql}appId and status = {_paramSql}status limit 1";
             return await QueryFirstOrDefaultAsync<ApplicationModel>(sql, new { appId, status = StatusEnum.Valid.GetHashCode() });
         }
 
         public async Task<bool> InsertAsync(ApplicationModel model)
         {
-            var sql = @$"insert into short_link_appication
+            var sql = @$"insert into short_link_application
                                          (app_code, app_name, remark,create_time,status,app_secret,update_time) 
                                    values
                                          ({_paramSql}app_code, {_paramSql}app_name, {_paramSql}remark, {_paramSql}create_time, {_paramSql}status, {_paramSql}app_secret, {_paramSql}update_time);";
@@ -60,13 +59,13 @@ namespace SuperShortLink.Repository
 
         public async Task<bool> UpdateStatusAsync(int appId, int status)
         {
-            var sql = @$"update short_link_appication set update_time ={_paramSql}update_time,status={_paramSql}status  where app_id = {_paramSql}appId";
+            var sql = @$"update short_link_application set update_time ={_paramSql}update_time,status={_paramSql}status  where app_id = {_paramSql}appId";
             return await base.ExecuteAsync(sql, new { appId, status = status, update_time = DateTime.Now }) > 0;
         }
 
         public async Task<bool> UpdateSecretAsync(int appId, string secret)
         {
-            var sql = $"update short_link_appication set update_time ={_paramSql}update_time,app_secret={_paramSql}secret  where app_id = {_paramSql}appId";
+            var sql = $"update short_link_application set update_time ={_paramSql}update_time,app_secret={_paramSql}secret  where app_id = {_paramSql}appId";
             return await base.ExecuteAsync(sql, new { appId, secret, update_time = DateTime.Now }) > 0;
         }
     }
